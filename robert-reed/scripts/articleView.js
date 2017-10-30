49c9c277bb70152c22d4ccf8c3ab3f760a4790b5
Robert Reed <robert.mc.reed@gmail.com>
2017-10-29 17:20:13 -0700

    fixed category/author data glitch

--- robert-reed/scripts/articleView.js
+++ robert-reed/scripts/articleView.js
@@ -61,16 +61,17 @@ articleView.setTeasers = () => {
   $('article').on('click', 'a.read-on', function(e) {
     e.preventDefault();
     // NOTE: Refactored 10 lines into 2. Woop!
-    $(this).parent().find('.article-body *:nth-of-type(n+2)').fadeToggle();
+    $(this).parent().find('.article-body').filter('*:nth-of-type(n+2)').fadeToggle();
+    console.log($(this).parent());
     $(this).text() === 'Read on →' ? $(this).html('Show Less &larr;') : $(this).html('Read on &rarr;');
   });
 };
 
-// COMMENT: Where is this function called? Why?
-// PUT YOUR RESPONSE HERE
+// COMMENTED: Where is this function called? Why?
+// This function is called at the bottom of new.html and is used to setup the default view of the page. This same js file is also referenced in index.html, so this approach allows us to pick and choose what gets run on each pageload.
 articleView.initNewArticlePage = () => {
-  // TODO: Ensure the main .tab-content area is revealed. We might add more tabs later or otherwise edit the tab navigation.
-
+  // DONE: Ensure the main .tab-content area is revealed. We might add more tabs later or otherwise edit the tab navigation.
+  $( '.tab-content' ).show();
 
   // TODO: The new articles we create will be copy/pasted into our source data file.
   // Set up this "export" functionality. We can hide it for now, and show it once we have data to export.
@@ -80,31 +81,38 @@ articleView.initNewArticlePage = () => {
   });
 
   // TODO: Add an event handler to update the preview and the export field if any inputs change.
-
 };
 
 articleView.create = () => {
-  // TODO: Set up a variable to hold the new article we are creating.
+  // DONE: Set up a variable to hold the new article we are creating.
   // Clear out the #articles element, so we can put in the updated preview
+  $( '#articles' ).html( '' );
+  // DONE: Instantiate an article based on what's in the form fields:
+  let d = new Date();
+  let newArticle = new Article( {
+    title: $( '#article-title' ).val(),
+    category: $( '#article-category' ).val(),
+    author: $( '#article-author' ).val(),
+    authorUrl: $( '#article-author-url' ).val(),
+    publishedOn: `${d.getFullYear()}-${d.getMonth() + 1}-${d.getDate()}`,
+    body: $( '#article-body' ).val()
+  } );
 
-
-  // TODO: Instantiate an article based on what's in the form fields:
-
-
-  // TODO: Use our interface to the Handblebars template to put this new article into the DOM:
-
+  // DONE: Use our interface to the Handblebars template to put this new article into the DOM:
+  $('#articles').append(newArticle.toHtml());
+  articleView.setTeasers();
 
   // TODO: Activate the highlighting of any code blocks; look at the documentation for hljs to see how to do this by placing a callback function in the .each():
-  $('pre code').each();
+  // $('pre code').each();
 
   // TODO: Show our export field, and export the new article as JSON, so it's ready to copy/paste into blogArticles.js:
 
 };
 
 // COMMENT: Where is this function called? Why?
-// PUT YOUR RESPONSE HERE
+// This function is called at the bottom of index.html and is used to setup the default view of the page. This same js file is also referenced in new.html, so this approach allows us to pick and choose what gets run on each pageload.
 articleView.initIndexPage = () => {
-  articles.forEach(article => $('#articles').append(article.toHtml()));
+  articles.forEach(article => $('#articles').append(article.toHtml())); // eslint-disable-line
   articleView.populateFilters();
   articleView.handleCategoryFilter();
   articleView.handleAuthorFilter();
